82151b71cc75e07f36ce9e47a60dbc0373aedb8f
ddiniz <ddiniz@student.42sp.org.br>
2022-06-09 23:20:44 -0300

    Update header file.

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,15 +6,25 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 17:55:35 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/05/30 19:03:45 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/06/08 21:16:17 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include "../libft/libft.h"
+#include <stdlib.h>
+#include <stdarg.h>
 
-int	ft_printf(void);
+#include "libft.h"
+
+typedef struct s_pack
+{
+	char	*str;
+	va_list	args;
+	size_t	len;
+}		t_pack;
+
+int	ft_printf(const char *str, ...);
 
 #endif
