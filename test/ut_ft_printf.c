75cc6a99945ba3b1da3b272493a6c9cd66612da4
ddiniz <ddiniz@student.42sp.org.br>
2022-06-10 17:18:28 -0300

    Branch: merge all.

---
+++ test/ut_ft_printf.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ut_ft_printf.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 17:18:47 by ddiniz            #+#    #+#             */
+/*   Updated: 2022/06/07 17:25:24 by ddiniz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+void	setUp(void)
+{
+
+}
+
+void	tearDown(void)
+{
+
+}
+
+void	ft_printf(void)
+{
+
+}
+
+void	main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST();
+	return UNITY_END();
+}
