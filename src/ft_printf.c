2d35a59525e6491c638663bf700bdef0749662b0
ddiniz <ddiniz@student.42sp.org.br>
2022-06-10 23:20:59 -0300

    Update printf core module.

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 17:45:58 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/06/10 17:24:29 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/06/10 17:30:00 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,9 @@ int	ft_printf(const char *str, ...)
 	while (*str)
 	{
 		if (*str != '%')
-		{
-			len += print_char();
-		}
+			len += print_char(*str);
 		elseif (*str == '%' && ft_strchar("cspdiuxX%", *(str + 1)))
-		{
-			len += check_char();
-		}
+			len += check_char(*(str + 1), args);
 		str++;
 	}
 	va_end(args);
